581c76543e3a707720c980087281697f0d9f5ab9
unknown <jawad13.karnib@gmail.com>
2024-10-22 17:46:47 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ZXingScannerComponent } from '@zxing/ngx-scanner'; // Import scanner component
+import { BarcodeFormat } from '@zxing/library'; // Import BarcodeFormat
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'barcode';
+  title = 'Barcode Scanner App';
+  scannedResult: string = '';
+  allowedFormats = [BarcodeFormat.QR_CODE, BarcodeFormat.CODE_128]; // Allowed formats
+  availableDevices: MediaDeviceInfo[] = [];
+  selectedDevice!: MediaDeviceInfo;
+  hasDevices = false;
+  hasPermission = false;
+
+  @ViewChild('scanner', { static: false }) scanner!: ZXingScannerComponent; // Reference to the scanner
+
+  ngOnInit() {
+    // Get available devices and check if we have camera permission
+    this.scanner.camerasFound.subscribe((devices: MediaDeviceInfo[]) => {
+      this.availableDevices = devices;
+      this.hasDevices = devices && devices.length > 0;
+    });
+
+    this.scanner.camerasNotFound.subscribe(() => {
+      console.error('No cameras found.');
+    });
+
+    this.scanner.permissionResponse.subscribe((perm: boolean) => {
+      this.hasPermission = perm;
+    });
+  }
+
+  startScan() {
+    if (this.availableDevices.length > 0) {
+      // Try to find the rear camera (usually the last one in the list)
+      const rearCamera = this.availableDevices.find(device => device.label.toLowerCase().includes('back')) || this.availableDevices[0];
+      this.selectedDevice = rearCamera;
+      this.scanner.device = this.selectedDevice; // Set the device for scanning
+    }
+  }
+
+  onCodeResult(resultString: string) {
+    this.scannedResult = resultString;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ZXingScannerModule } from '@zxing/ngx-scanner';
 
 @NgModule({
   declarations: [
@@ -10,7 +9,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ZXingScannerModule // Correct module import
   ],
   providers: [],
   bootstrap: [AppComponent]
